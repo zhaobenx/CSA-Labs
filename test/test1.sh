0e239abf4558c062e70c86fda17c2cde1fce745e
Ben <ghostbenzhao@gmail.com>
2019-10-28 22:09:13 -0400

    Add test1 script

---
+++ test/test1.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+########
+src_dir="../Lab 1"
+src_name="MIPS_pipeline.cpp"
+test_dir="test1"
+temp="temp"
+execuable="Lab1"
+
+
+$CXX $src_dir/$src_name -o $execuable
+
+# cd $temp
+
+# rm -f dmem.txt 
+# rm -f dmemresult_answer.txt
+# rm -f imem.txt
+# rm -f Instructions.txt
+# rm -f RFresult_answer.txt
+
+successful=0
+failed=0
+
+mkdir $temp
+for dir in ./$test_dir/*
+do
+
+	for file in $dir/*
+	do
+		cp $file ./$temp
+	done
+	cp $execuable ./$temp/$execuable
+
+	cd $temp
+	if [ "$1" = "show" ]; then
+		./$execuable 
+	else
+		./$execuable > /dev/null
+	fi
+
+
+	cmp RFresult_ans.txt  RFresult.txt -s
+	s1=$?
+	cmp dmemresult_ans.txt dmemresult.txt -s
+	s2=$?
+	if (($s1))|| (($s2));
+	then
+		tput setaf 1
+		((failed++))
+	else
+		tput setaf 2	
+		((successful++))
+	fi
+	
+	echo testing $dir
+	tput sgr0
+
+	rm *
+	cd ..
+	# rm -f dmem.txt 
+	# rm -f dmemresult_answer.txt
+	# rm -f dmemresult.txt
+	# rm -f imem.txt
+	# rm -f Instructions.txt
+	# rm -f RFresult_answer.txt
+	# rm -f RFresult.txt
+	
+done
+
+rm -r $temp
+rm $execuable
+echo $successful successful, $failed failed
+
+if [[ $failed -gt 0 ]]; 
+then
+	exit 1;
+else
+	exit 0;
+fi
